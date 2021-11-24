8c02da42dea4540cef08627ebfc3c8cd882015c7
Chi-Li <cchiu@chanzuckerberg.com>
2021-11-24 14:50:26 -0800

    barebone

--- src/napari_labels_overlap/_dock_widget.py
+++ src/napari_labels_overlap/_dock_widget.py
@@ -1,41 +1,28 @@
 """
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the ``napari_experimental_provide_dock_widget`` hook specification.
-see: https://napari.org/docs/dev/plugins/hook_specifications.html
-
-Replace code below according to your needs.
+Input: two labels layers
+Output: labels layer
 """
 from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
 from magicgui import magic_factory
+from napari.types import LabelsData
 
+from enum import Enum
+import numpy as np
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
+#TO DO
+class Operation(Enum):
+    a_or_b = np.add
+    a_and_b = np.add
 
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def labels_overlap(layerA: LabelsData, layerB: LabelsData, operation: Operation) -> LabelsData:
+    if layerA is not None and layerB is not None:
+        return (operation.value(layerA, layerB))
+    else:
+        #TO DO: add error message
+        return 0
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    # you can return either a single widget, or a sequence of widgets
-    return [ExampleQWidget, example_magic_widget]
+    return [labels_overlap]
