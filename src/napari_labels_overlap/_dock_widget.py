68b6d4a8c2512276a307cec03b3784c91e1e3ce4
Chi-Li <cchiu@chanzuckerberg.com>
2022-01-06 10:51:47 -0800

    add label output options

--- src/napari_labels_overlap/_dock_widget.py
+++ src/napari_labels_overlap/_dock_widget.py
@@ -2,11 +2,13 @@
 Input: two labels layers
 Output: labels layer
 """
-from napari_plugin_engine import napari_hook_implementation
+
 from napari.types import LabelsData
 from enum import Enum
 from functools import partial
+from skimage.morphology import label
 
+#Operation
 def aORb(a,b):
      return (a+b > 0).astype(int)
 
@@ -16,6 +18,12 @@ def aANDb(a,b):
 def aNOTb(a,b):
     return (a-b > 0).astype(int)
 
+#output_type
+def binary_output(ab):
+    return ab
+def connected_output(ab):
+    return label(ab)
+
 # Enums check if the value of the member is a descriptor, and if so, don't add it
 # partial() objects are not descriptors (don't have __get__, __set__, or __delete__), and therefore sneak through Enum's check
 class Operation(Enum):
@@ -23,13 +31,15 @@ class Operation(Enum):
     A_AND_B = partial(aANDb)
     A_NOT_B = partial(aNOTb)
 
-def labels_overlap(layerA: LabelsData, layerB: LabelsData, operation: Operation) -> LabelsData:
+class Output(Enum):
+    binary = partial(binary_output)
+    connected_component = partial(connected_output)
+
+def labels_overlap(layerA: LabelsData, layerB: LabelsData, operation: Operation, output:Output) -> LabelsData:
     if layerA is not None and layerB is not None:
         bin_A = (layerA > 0).astype(int)
         bin_B = (layerB > 0).astype(int)
-        return operation.value(bin_A, bin_B)
+        return output.value(operation.value(bin_A, bin_B))
 
-
-#@napari_hook_implementation
 def napari_experimental_provide_function():
     return [labels_overlap]
